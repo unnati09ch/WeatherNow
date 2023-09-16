eddde5cae975280e6baf766a500f673cdd7701cf
unnati09ch <141519277+unnati09ch@users.noreply.github.com>
2023-09-16 18:57:40 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b21427031b5589217155806ca328e092",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
